14719b46fefce90f8712a4492a8777e8059ab4ca
linklee <nevernight721@gmail.com>
2017-01-14 23:06:03 +0500

    python

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ from flask import Flask, render_template, request, jsonify, redirect
 from mailer import send_message,send_message_self
 from validator import Validator
 import urllib
-from urlparse import urlparse
+try:
+    from urllib.parse import urlparse
+except ImportError:
+     from urlparse import urlparse
+
 
 
 app = Flask(__name__,
