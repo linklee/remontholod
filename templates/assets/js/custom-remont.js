d3ac8d28069759a089be8039a22955d4b1917037
linklee <nevernight721@gmail.com>
2017-01-22 22:47:44 +0500

    changes

--- templates/assets/js/custom-remont.js
+++ templates/assets/js/custom-remont.js
@@ -1,6 +1,7 @@
 $(function() {
-    $('.hp-trigger').click(function(e) {
+    $('.hp-trigger').click(function(event) {
+	    event.preventDefault();
         $('.hidden-price').toggleClass('hidden');
-     	e.stopPropagation()
+        $('html,body').animate({scrollTop:$('#hidden-price').offset().top}, 500);
     })
 });
